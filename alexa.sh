ea3c3975eaea75f5b4e3b47796dc5d0b0d819b94
Patrik Thunström <magebarf@gmail.com>
2018-01-02 02:33:00 +0100

    Add traceroute logging from the parsed list

--- alexa.sh
+++ alexa.sh
@@ -3,6 +3,9 @@
 # Downloads and parses the top 50 list from alexa
 # to a single file containing only the top domain URLs
 
+# Then performs a traceroute for each of those domains
+# logging the results to individual files
+
 usage() {
 	echo usage: `basename $0` 'runname' 1>&2
 	exit 1
@@ -20,3 +23,10 @@ errquit() {
 
 mkdir $1
 curl --url https://www.alexa.com/topsites | grep "href=\"/siteinfo/" | sed 's/<a href="\/siteinfo\/\(.*\)">.*/\1/g' > $1/sitelist
+
+# Additional arguments:
+#	-a for AS# resolution
+# 	-w 2 for shorter than default (5) timeout
+while read u; do
+	traceroute -a -w 2 $u > $1/$u.trace
+done <$1/sitelist
